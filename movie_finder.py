494db214586218d538f1d83146dcdfc600d0e0ff
Gokul Viswanath <viswanath1gokul@gmail.com>
2021-04-01 22:35:42 +0530

    env variable change

--- movie_finder.py
+++ movie_finder.py
@@ -4,7 +4,7 @@ from flask import json, jsonify
 
 class MovieFinder:
     def __init__(self):
-        self.api_key = os.environ.get("API_KEY")
+        self.api_key = os.environ.get("MOVIEDB_API_KEY")
 
     def get_trending(self):
         response = requests.get(f"https://api.themoviedb.org/3/trending/all/week?api_key={self.api_key}")
